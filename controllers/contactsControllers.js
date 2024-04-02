ab0de8b89e98e2f5e83d829e60bd63d35148e433
ValentynHromadskyi <126354727+ValentynHromadskyi@users.noreply.github.com>
2024-04-02 20:29:59 +0300

    fix-bugs

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,9 +4,7 @@ import * as contactsService from "../services/contactsServices.js";
 
 export const getAllContacts = catchAsync(async (req, res) => {
   const result = await contactsService.listContacts();
-  res.status(200).json({
-    result,
-  });
+  res.status(200).json(result);
 });
 
 export const getOneContact = catchAsync(async (req, res) => {
@@ -15,9 +13,7 @@ export const getOneContact = catchAsync(async (req, res) => {
   if (!result) {
     throw HttpError(404);
   }
-  res.status(200).json({
-    result,
-  });
+  res.status(200).json(result);
 });
 
 export const deleteContact = catchAsync(async (req, res) => {
@@ -26,18 +22,14 @@ export const deleteContact = catchAsync(async (req, res) => {
   if (!result) {
     throw HttpError(404);
   }
-  res.status(200).json({
-    result,
-  });
+  res.status(200).json(result);
 });
 
 export const createContact = catchAsync(async (req, res) => {
   console.log(req);
   const result = await contactsService.addContact(req.body);
 
-  res.status(201).json({
-    result,
-  });
+  res.status(201).json(result);
 });
 
 export const updateContact = catchAsync(async (req, res) => {
@@ -46,7 +38,8 @@ export const updateContact = catchAsync(async (req, res) => {
   if (!result) {
     throw HttpError(404);
   }
-  res.status(200).json({
-    result,
-  });
+  if (Object.keys(req.body).length === 0) {
+    return res.status(400).json({ error: "Body must have at least one field" });
+  }
+  res.status(200).json(result);
 });
