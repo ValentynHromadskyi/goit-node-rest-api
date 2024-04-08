e6b6db68fe85faa7f4ad2eb290654a3f19b82ff7
ValentynHromadskyi <126354727+ValentynHromadskyi@users.noreply.github.com>
2024-04-08 16:21:56 +0300

    hw-03

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-
+import mongoose from "mongoose";
 import contactsRouter from "./routes/contactsRouter.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
 
 app.use(morgan("tiny"));
 app.use(cors());
---
+++ models/contactsModel.js
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+    },
+    favorite: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { versionKey: false }
+);
+
+export const Contact = model("Contact", contactsSchema);
